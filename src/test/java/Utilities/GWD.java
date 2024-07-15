fd8396bde9774c7f839c00647ad7ca402716005e
Burak TABANLI <b.tabanli93@hotmail.com>
2024-07-15 15:47:45 +0200

    GWD update for parallel testing. Xml ready to use.

--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -1,7 +1,9 @@
 package Utilities;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -9,37 +11,62 @@ import java.util.Locale;
 
 public class GWD {
 
-
-    private static WebDriver driver;
-
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+    public static ThreadLocal<String> threadBrowserName = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
 
+        //language settings for non english computers. you need do add it if not extent report won't work.
         Locale.setDefault(new Locale("EN"));
         System.setProperty("user.language", "EN");
 
-        //if driver empty it needs to work
-        if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        if (threadBrowserName.get() == null) {
+            threadBrowserName.set("firefox");
         }
 
-        return driver;
+        if (threadDriver.get() == null) {
+            switch (threadBrowserName.get()) {
+                case "firefox":
+                    threadDriver.set(new FirefoxDriver());
+                    break;
+                case "edge":
+                    threadDriver.set(new EdgeDriver());
+                    break;
+                case "chrome":
+                    threadDriver.set(new ChromeDriver());
+                    break;
+                default:
+                    threadDriver.set(new FirefoxDriver());
+
+            }
+        }
+
+        threadDriver.get().manage().window().maximize();
+        threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        return threadDriver.get();
     }
 
 
     public static void quitDriver() {
+
         try {
             Thread.sleep(5000);
-        }catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        if (driver != null) {
-            driver.quit();
+        if(threadDriver.get()!=null)
+        {
+            threadDriver.get().quit();
+
+            WebDriver driver = threadDriver.get();
             driver = null;
+
+            threadDriver.set(driver);
         }
+
     }
 
 }
+
+
