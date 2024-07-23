c29d732b608beadc12a5335f56a27f214e7dd7da
Burak TABANLI <b.tabanli93@hotmail.com>
2024-07-23 21:38:13 +0200

    ConfigReader ready to use

---
+++ src/test/java/Utilities/ConfigReader.java
@@ -0,0 +1,31 @@
+package Utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    public static Properties getPropertyObject() throws IOException {
+        FileInputStream fp = new FileInputStream("Config/config.properties");
+        Properties properties = new Properties();
+        properties.load(fp);
+        return properties;
+
+    }
+
+    public static String getUrl() throws IOException {
+
+        return getPropertyObject().getProperty("url");
+    }
+
+    public static String getUsername() throws IOException {
+
+        return getPropertyObject().getProperty("username");
+    }
+
+    public static String getPassword() throws IOException {
+        return getPropertyObject().getProperty("password");
+    }
+
+}
